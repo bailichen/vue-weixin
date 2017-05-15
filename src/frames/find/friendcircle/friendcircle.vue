5c2f8809076da813271be80e2dce84c86f868138
unknown <xulj@sun3d.com>
2017-05-15 18:57:21 +0800

    firend

--- src/frames/find/friendcircle/friendcircle.vue
+++ src/frames/find/friendcircle/friendcircle.vue
@@ -1,10 +1,10 @@
 <template>
 	<section class="child_page">
 		<head-top crossover="朋友圈"></head-top>
-		<section class="friend">
-			<div class="theme" @click="exportInput">
+		<section class="friend" ref="friend" @scroll="friendScroll">
+			<div class="theme" >
 				<div class="themeinit"></div>
-				<img :src="newImg" id="imgSrc" ref="imgSrc" class="imgSrc" />
+				<div :class="{shoowimg : imageSrc}"><img :src="newImg" id="imgSrc" ref="imgSrc" class="imgSrc" @click="exportInput" /></div>
 				<div class="themetext" :class="{shoowimg : imageSrc}">轻触更换主题照片</div>
 				<div class="personImg">
 					<div class="personame">{{userInfo.petname}}</div>
@@ -24,7 +24,7 @@
 			</div>
 			<div class="condition">
 				<ul>
-					<li>
+					<li class="condition_li">
 						<div class="condition_left">
 							<img src="../../../images/b1.jpg" alt="">
 						</div>
@@ -44,9 +44,49 @@
 									<span>删除</span>
 								</div>
 								<div class="button_right">
-									<svg class="button_svg">
+									<svg class="button_svg" @click="showDiscuss">
 										<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#comment"></use>
 									</svg>
+									<div class="discuss" v-if="criticism" :class="{discusshow : reviewshow, discusshide : reviewhide}">
+										<div>
+											<svg fill="#fff">
+												<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#like"></use>
+											</svg>
+											<span>赞</span>
+										</div>
+										<div>
+											<svg fill="#fff">
+												<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#discuss"></use>
+											</svg>
+											<span>评论</span>
+										</div>
+									</div>
+									
+								</div>
+							</div>
+							<div class="retext">
+								<svg class="retext_trigon" fill="#efefef">
+									<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#trigon"></use>
+								</svg>
+								<div class="retext_like clear" :class="{likeborder : bordercss}">
+									<svg class="retext_like_svg" fill="#8792b0">
+										<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#like"></use>
+									</svg>
+									<span>徐连杰,</span>
+									<span>徐连杰,</span>
+									<span>徐连杰,</span>
+									<span>徐连杰,</span>
+									<span>徐连杰,</span>
+									<span>徐连杰,</span>
+									<span>徐连杰,</span>
+									<span>徐连杰,</span>
+								</div>
+								<div class="retext_revert">
+									<ul>
+										<li>
+											<span>名字</span>：嗯嗯嗯嗯嗯嗯嗯嗯嗯嗯嗯嗯嗯嗯嗯嗯嗯嗯嗯嗯嗯嗯嗯嗯嗯嗯嗯嗯嗯嗯
+										</li>
+									</ul>
 								</div>
 							</div>
 						</div>
@@ -65,14 +105,23 @@
 		data(){
 			return{
 				filevalue:'',
-				imageSrc:false,
-				afterclcik:true,
-				releaseimgnum:true,
+				imageSrc:false,			//显示的是图片还是文字
+				afterclcik:true,		//点击显示上传图片的input
+				releaseimgnum:true,		//上传的图片数是否大于1
+				reviewshow:false,		//评论的弹层显示时的动画
+				reviewhide:false,		//评论的弹层消失时的动画
+				criticism:false,		//评论的弹层是否显示
+				flag:true,				//点击判断
+				timer:null,
+				bordercss:true,			//点赞的下边框
 			}
 		},
 		created(){
 
 		},
+		beforeDestroy(){
+            clearTimeout(this.timer)
+        },
 		mounted(){
 			//上传图片并展示图片（无剪裁功能）
 			new uploadPreview({
@@ -87,6 +136,8 @@
 				}
 			});
 			this.getUserInfo();
+			console.log(this.$refs.clickdom)
+			
 		},
 		components:{
 			headTop,
@@ -109,6 +160,32 @@
 			hideIput(){
 				this.afterclcik=true;
 				this.SAVE_THEMIMG(this.$refs.imgSrc.src)
+			},
+			commentShow(){
+				this.criticism=true;
+				this.reviewshow=true;
+				this.reviewhide=false;
+				this.flag=false;
+			},
+			commentHide(){
+				this.reviewshow=false;
+				this.reviewhide=true;
+				this.timer=setTimeout(() => {
+					clearTimeout(this.timer);
+					this.criticism=false;
+				},1000)
+				this.flag=true;
+			},
+			showDiscuss(){
+				if(this.flag){
+					this.commentShow()
+				}else{
+					this.commentHide();
+				}
+			},
+			friendScroll(){
+				console.log(this.$refs.friend.scrollTop)
+				this.$refs.friend.scrollTop=0;
 			}
 		}
 	}
@@ -126,6 +203,8 @@
 	}
 	.friend{
 		padding-top:2.06933rem;
+		height:28rem;
+		overflow:auto;
 		background-color: #f8f8f8;
 		.theme{
 			width:100%;
@@ -202,8 +281,7 @@
 				line-height:2.048rem;
 				background:#fff;
 				border-radius:3px;
-				@include sizeColor(0.64rem,#333);
-				
+				@include sizeColor(0.64rem,#333);	
 				.wipeinput{
 					position: relative;
 					padding-left:1rem;
@@ -228,7 +306,7 @@
 			padding-top:1.5786666667rem;
 			ul{
 				width:100%;
-				li{
+				.condition_li{
 					padding:0.512rem;
 					border-bottom:1px solid #e2e2e2;
 					@include justify(flex-start);
@@ -245,7 +323,7 @@
 						h1{
 							display:block;
 							padding-top:0.1706666667rem;
-							@include sizeColor(0.5546666667rem,#565b71);
+							@include sizeColor(0.5546666667rem,#8792b0);
 						}
 						.publishtext{
 							margin-top:0.064rem;
@@ -283,15 +361,92 @@
 									margin-right:0.4266666667rem;
 								}
 								span+span{
-									color:#565b71;
+									color:#8792b0;
 								}
 							}
 							.button_right{
 								margin-top:0.6826666667rem;
+								position: relative;
 								@include widthHeight(0.9386666667rem,0.64rem);
 								.button_svg{
 									display:block;
 									@include widthHeight(100%,100%);
+								}
+								.discuss{
+									position: absolute;
+									@include widthHeight(8.2346666667rem,1.7066666667rem);
+									background:#373b3e;
+									border-radius:3px;
+									right:1.408rem;
+									top:-0.5973333333rem;
+									box-sizing: border-box;
+									@include align;
+									div{
+										width:50%;
+										float:left;
+										@include justify(center);
+
+										svg{
+											display:block;
+											@include widthHeight(0.768rem,0.768rem);
+											margin-right:0.2133333333rem;
+										}
+										span{
+											display:block;
+											@include sizeColor(0.5546666667rem,#fff);
+										}
+									}
+									div:first-child{
+										border-right:2px solid #2f3336;
+									}
+								}
+								.discusshow{
+									animation: flipInX 1s 1 ease-in-out both;
+								}
+								.discusshide{
+									animation: flipOutX 1s 1 ease-in-out both;
+								}
+								
+							}
+						}
+						.retext{
+							
+							margin-top:0.128rem;
+							.retext_trigon{
+								display:block;
+								@include widthHeight(.8rem,.4rem);
+								margin-left:0.4266666667rem;
+							}
+							.retext_like{
+								background:#efefef;
+								padding:0.3413333333rem;
+								.retext_like_svg{
+									float:left;
+									@include widthHeight(0.512rem,0.512rem);
+									margin-right:0.2133333333rem;
+									margin-top:0.064rem;
+								}
+								span{
+									float:left;
+									margin-right:0.2133333333rem;
+									@include sizeColor(0.512rem,#8792b0);
+								}
+							}
+							.likeborder{
+								border-bottom:1px solid #e2e2e2;
+							}
+							.retext_revert{
+								background:#efefef;
+								ul{
+									li{
+										border:0;
+										padding:0.3413333333rem;
+										@include sizeColor(0.5546666667rem,#333);
+										span{
+											display:inline-block;
+											color:#8792b0;
+										}
+									}
 								}
 							}
 						}
@@ -300,5 +455,56 @@
 			}
 
 		}
+	}
+
+	@keyframes flipInX {
+	  from {
+	    -webkit-transform: perspective(400px) rotate3d(1, 0, 0, 90deg);
+	    transform: perspective(400px) rotate3d(1, 0, 0, 90deg);
+	    -webkit-animation-timing-function: ease-in;
+	    animation-timing-function: ease-in;
+	    opacity: 0;
+	  }
+
+	  40% {
+	    -webkit-transform: perspective(400px) rotate3d(1, 0, 0, -20deg);
+	    transform: perspective(400px) rotate3d(1, 0, 0, -20deg);
+	    -webkit-animation-timing-function: ease-in;
+	    animation-timing-function: ease-in;
+	  }
+
+	  60% {
+	    -webkit-transform: perspective(400px) rotate3d(1, 0, 0, 10deg);
+	    transform: perspective(400px) rotate3d(1, 0, 0, 10deg);
+	    opacity: 1;
+	  }
+
+	  80% {
+	    -webkit-transform: perspective(400px) rotate3d(1, 0, 0, -5deg);
+	    transform: perspective(400px) rotate3d(1, 0, 0, -5deg);
+	  }
+
+	  100% {
+	    -webkit-transform: perspective(400px);
+	    transform: perspective(400px);
+	  }
+	}
+	@keyframes flipOutX {
+	  from {
+	    -webkit-transform: perspective(400px);
+	    transform: perspective(400px);
+	  }
+
+	  30% {
+	    -webkit-transform: perspective(400px) rotate3d(1, 0, 0, -20deg);
+	    transform: perspective(400px) rotate3d(1, 0, 0, -20deg);
+	    opacity: 1;
+	  }
+
+	  100% {
+	    -webkit-transform: perspective(400px) rotate3d(1, 0, 0, 90deg);
+	    transform: perspective(400px) rotate3d(1, 0, 0, 90deg);
+	    opacity: 0;
+	  }
 	}
 </style>
