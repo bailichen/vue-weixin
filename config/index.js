cc7d96573c0905686fb75aeaaec1dad408f20614
maguohua <1264889788@qq.com>
2017-06-14 20:38:50 +0800

    fix some bugs

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
         index: path.resolve(__dirname, '../weixin/index.html'),
         assetsRoot: path.resolve(__dirname, '../weixin'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: '/weixin/',
+        assetsPublicPath: '/',
         productionSourceMap: true,
         // Gzip off by default as many popular static hosts such as
         // Surge or Netlify already gzip all static assets for you.
